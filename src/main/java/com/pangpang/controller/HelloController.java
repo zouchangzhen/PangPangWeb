00752c264b2e6100994839d4ff3f2de4c99aaecb
Administrator <745596015@qq.com>
2017-09-05 19:14:48 +0800

    配置日志输出

--- src/main/java/com/pangpang/controller/HelloController.java
+++ src/main/java/com/pangpang/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.pangpang.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
-    @ResponseBody
     String home() {
+        logger.info("请求");
         return "Hello World!";
     }
 }
--- src/main/java/com/pangpang/controller/OrderController.java
+++ src/main/java/com/pangpang/controller/OrderController.java
@@ -4,25 +4,23 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.pangpang.pojo.Orders;
 import com.pangpang.service.OrdersService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class OrderController {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private OrdersService orderService;
 
-    @RequestMapping("/getOrders/{pageNum}/{pageSize}")
-    @ResponseBody
+    @RequestMapping(value = "/getOrders/{pageNum}/{pageSize}",method = RequestMethod.GET)
     public PageInfo<Orders> getOrders(@PathVariable("pageNum") int pageNum,@PathVariable("pageSize") int pageSize,String buyer,String date) {
-
+        logger.info("请求");
         PageHelper.startPage(pageNum,pageSize);
         List<Orders> orders=orderService.getAll();
 
@@ -31,28 +29,30 @@ public class OrderController {
         return pOrders;
     }
 
-    @RequestMapping("/getOrder")
-    @ResponseBody
+    @RequestMapping(value = "/getOrder",method = RequestMethod.GET)
     public Orders getOrder(Long id) {
+        logger.info("请求");
         Orders order=orderService.getOne(id);
         return order;
     }
 
-    @RequestMapping("/addOrder")
-    @ResponseBody
+    @RequestMapping(value = "/addOrder",method = RequestMethod.GET)
     public void save(Orders orders) {
+
+        logger.info("请求");
         orderService.insert(orders);
     }
 
-    @RequestMapping(value="updateOrder")
-    @ResponseBody
+    @RequestMapping(value="updateOrder",method = RequestMethod.GET)
     public void update(Orders orders) {
+
+        logger.info("请求");
         orderService.update(orders);
     }
 
-    @RequestMapping(value="/deleteOrder/{id}")
-    @ResponseBody
+    @RequestMapping(value="/deleteOrder/{id}",method = RequestMethod.GET)
     public void delete(@PathVariable("id") Long id) {
+        logger.info("请求");
         orderService.delete(id);
     }
 
